3f05842028ba282023e74e5573e78a6823d8d1d9
caongan <caothuongngan@gmail.com>
2021-08-08 23:16:26 +0700

    ....

--- resources/views/customer/comment.blade.php
+++ resources/views/customer/comment.blade.php
@@ -45,7 +45,7 @@
                                             <span class="username">
                                             <a href="#">{{$comment->user->name}}</a>
                                                 @if(\Illuminate\Support\Facades\Auth::user()->name == $comment->user->name)
-                                                    <a href="{{route('comment.destroy', $comment->id)}}">xoas</a>
+                                                    <a style="background-color: white" href="{{route('comment.destroy', $comment->id)}}">xoas</a>
 {{--                                                    <button type="button" data-id="{{$comment->id}}" class="delete-comment float-right btn-tool"><i class="fas fa-times"></i></button>--}}
                                                 @endif
                                             </span>
